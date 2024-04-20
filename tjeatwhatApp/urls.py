6e20ebedd0f2c847c43b71cd078d7154a0368bd6
coder-gx <1829795398@qq.com>
2024-04-21 01:06:18 +0800

    change urls

--- tjeatwhatApp/urls.py
+++ tjeatwhatApp/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('restaurant/', views.create_restaurant,name="create_restaurant"),
-    path('restaurant/<int:id>/update',  views.update_restaurant,name="update_restaurant"),
-    path('restaurant/<int:id>/delete',  views.delete_restaurant,name="delete_restaurant"),
-    path('dish/<int:id>/', views.create_dish,name="create_dish"),
-    path('dish/<int:id>/<int:dish_id>/update',  views.update_dish,name="update_dish"),
-    path('dish/<int:id>/<int:dish_id>/delete',  views.delete_dish,name="delete_dish"),
+    path('restaurant/create', views.create_restaurant,name="create_restaurant"),
+    path('restaurant/<int:id>/update/',  views.update_restaurant,name="update_restaurant"),
+    path('restaurant/<int:id>/delete/',  views.delete_restaurant,name="delete_restaurant"),
+    path('restaurant/<int:id>/create', views.create_dish,name="create_dish"),
+    path('restaurant/<int:id>/<int:dish_id>/update/',  views.update_dish,name="update_dish"),
+    path('restaurant/<int:id>/<int:dish_id>/delete/',  views.delete_dish,name="delete_dish"),
     path('image/',  views.upload_file,name="upload_image"),
     
 ]
